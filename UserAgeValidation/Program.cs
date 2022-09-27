a6609dca12332aff51f1ae272cbe07efd0e9ed39
Greete <greetecaif@gmail.com>
2022-09-27 21:58:50 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,17 @@
+﻿// rakendus küsib kasutaja sugu ja perekonnanime, loob lähtuvalt sisendist tervituse
+Console.WriteLine("Please enter your surname: ");
+string userSurname = Console.ReadLine();
+Console.WriteLine("Please select your gender (m/f):");
+char userGender = Char.Parse(Console.ReadLine());
+if(userGender=='m')
+{
+    Console.WriteLine($"Welcome, Mr. {userSurname}!");
+}
+else if(userGender=='f')
+{
+    Console.WriteLine($"Welcome, Ms. {userSurname}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userSurname}!");
+}
---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,11 @@
+﻿//rakendus küsib kasutaja vanust; kui vanus < 13, siis konsoolis kuvatakse teade, muul juhul tervitus
+Console.WriteLine("Enter your age:");
+int userAge = Int32.Parse(Console.ReadLine());
+if(userAge >= 13)
+{
+    Console.WriteLine("Welcome to Instagram!");
+}
+else
+{ 
+    Console.WriteLine("You are too young to use Instagram.");
+}
